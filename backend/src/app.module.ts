0037d59b5d0f7fbb7e52ef1f8b21eeb10ef0eef0
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2025-09-20 23:23:30 +0700

    chore: konfigurasi dasar dan setup seeders

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({}), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getHello(): any {
+    const user = this.prisma.user.findMany({});
+
+    return user;
     return 'Hello World!';
   }
 }
