774ec644987ee904e54b634bd53640ec342799d1
Ahmad Zidni Hidayat <138422717+ahmaddzidnii@users.noreply.github.com>
2025-09-24 18:24:19 +0700

    merge main ke fi/fix-auth-module (#4)
    * Add: card, input, and label component
    
    * Add: logo UIN
    
    * Add: Login Page (Belum Fix kayanya)
    
    * Fix: Size Logo
    
    ---------
    
    Co-authored-by: Ozin-kun <rozingunagraha@gmail.com>
    Co-authored-by: Rozin Gunagraha <98881560+Ozin-kun@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,11 @@
-import { useState } from "react";
+import LoginPage from "@/components/pages/LoginPage";
 
-import "./styles/global.css";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import { Button } from "./components/ui/button";
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="max-w-5xl mx-auto p-8 text-center">
-      {/* Logo Section */}
-      <div className="flex justify-center gap-8">
-        <a
-          href="https://vite.dev"
-          target="_blank"
-        >
-          <img
-            src={viteLogo}
-            alt="Vite logo"
-            className="h-24 transition hover:drop-shadow-[0_0_2em_#646cffaa]"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            alt="React logo"
-            className="h-24 transition hover:drop-shadow-[0_0_2em_#61dafbaa] animate-spin-slow"
-          />
-        </a>
-      </div>
-
-      {/* Title */}
-      <h1 className="mt-8 text-4xl font-bold">Vite + React + Tailwind</h1>
-
-      {/* Card Section */}
-      <div className="mt-6 p-6 border rounded-xl shadow bg-white/5">
-        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
-        <p className="mt-4 text-gray-600">
-          Edit <code className="font-mono">src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      {/* Footer */}
-      <p className="mt-6 text-gray-500">Click on the Vite and React logos to learn more</p>
-    </div>
+    <>
+      <LoginPage />
+    </>
   );
 }
 
